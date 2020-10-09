9c73c58352b40bcebc6bc0c3762b7a454f357b28
StarCoral <qbe841501@gmail.com>
2020-10-09 13:28:31 +0800

    let the update-codegen.sh more general

--- Operator/podMonitor/hack/k8s/update-codegen.sh
+++ Operator/podMonitor/hack/k8s/update-codegen.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-GO111MODULE=off ./generate-groups.sh all \
+GO111MODULE=off $GOPATH/src/k8s.io/code-generator/generate-groups.sh all \
      github.com/NTHU-LSALAB/podMonitor/pkg/generated \
      github.com/NTHU-LSALAB/podMonitor/pkg/apis \
      podmonitor:v1 \
-     --go-header-file ~/src/github.com/NTHU-LSALAB/podMonitor/hack/k8s/boilerplate.go.txt
+     --go-header-file $GOPATH/src/github.com/NTHU-LSALAB/podMonitor/hack/k8s/boilerplate.go.txt
